8689e2659ee5fd05b39315b15941446692e77952
adhayes1 <adhayes1@memphis.edu>
2019-02-20 19:34:26 -0500

    I don't know what I did but it wouldn't push...

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,8 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 //notice for our component import we are using the default import from the container file
 //we need to do this so that redux can connect our component to the store and actions
-<<<<<<< HEAD
-import LoginComponent from './components/login/Login.container';
-=======
 import  LoginComponent  from './components/login/Login.container';
 import RegisterComponent  from './components/register/Register.container';
->>>>>>> f24be5e1e5c1a1a19b8e08384fa9fcc358ec0667
 
 class App extends Component {
   render() {
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -25,22 +25,13 @@ export interface IRegisterState{
 //We make it out of other more specific interfaces.
 //Also, every interface here needs a reducer of the same name down in combine reducers
 export interface IState {
-<<<<<<< HEAD
-    login: ILoginState
-
-=======
     login: ILoginState,
     register: IRegisterState
->>>>>>> f24be5e1e5c1a1a19b8e08384fa9fcc358ec0667
 }
 
 
 //This needs a reducer with the name of every interface we have in the IState interface
 export const state = combineReducers<IState>({
-<<<<<<< HEAD
-    login: loginReducer
-=======
     login: loginReducer,
     register: registerReducer,
->>>>>>> f24be5e1e5c1a1a19b8e08384fa9fcc358ec0667
 })
