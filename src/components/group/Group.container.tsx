11db8ed7b8d8fc61433f272c4fd8358ef2d7c0da
adhayes1 <adhayes1@memphis.edu>
2019-02-28 22:43:52 -0500

    done?

--- src/components/group/Group.container.tsx
+++ src/components/group/Group.container.tsx
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
 import { IState } from "../../reducers";
 import { GroupComponent } from "./Group.component";
-import { setCurrentGroup, getAllGroups, addReceipt, updateReceiptName, addLineToReceiptButton, addLineToReceipt, updateNameLineToAdd, resetAddLineForm, updateLinePriceToAdd, resetAddLineNameForm, resetAddLinePriceForm } from "../../actions/Group/Group.action";
+import { setCurrentGroup, getAllGroups, addReceipt, updateReceiptName, addLineToReceipt, updateNameLineToAdd, resetAddLineForm, updateLinePriceToAdd, resetAddLineNameForm, resetAddLinePriceForm } from "../../actions/Group/Group.action";
 import { Groups } from "../../models/Groups";
 
 const mapStateToProps = (state: IState) => {
@@ -16,7 +16,6 @@ const mapStateToProps = (state: IState) => {
     getAllGroups,
     addReceipt,
     updateReceiptName,
-    addLineToReceiptButton,
     addLineToReceipt,
     updateNameLineToAdd,
     resetAddLineForm,
