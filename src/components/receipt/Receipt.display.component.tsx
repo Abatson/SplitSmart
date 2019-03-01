566a5ec39c194e619798436211cb7cf6d762eaef
csfrancis94 <csfrancis@email.com>
2019-02-28 21:12:29 -0500

    Something different

--- src/components/receipt/Receipt.display.component.tsx
+++ src/components/receipt/Receipt.display.component.tsx
@@ -25,10 +25,10 @@ export class ReceiptDisplayComponent extends React.Component<any, any> {
       <p>
       {(this.props.receipt.claimant == -1) ? "No one has claimed this receipt!" : "Receipt Claimant:"}   {(this.props.receipt.receiptClaimant.username == "") ? "" : this.props.receipt.receiptClaimant.username}  
       </p>
-      <p>
-      Claim Receipt:  <input onClick={this.props.onClick1} type="checkbox" id="scales" name="scales"
-     ></input>
-      </p>
+      {/* <p> */}
+      {/* Claim Receipt:  <input onClick={this.props.onClick1} type="checkbox" id="scales" name="scales" */}
+     {/* ></input> */}
+      {/* </p> */}
       <p>
       Purchases: 
       </p>
@@ -39,14 +39,14 @@ export class ReceiptDisplayComponent extends React.Component<any, any> {
       for (let i = 0; i < this.props.receipt.lines.length; i++)
       {
         tempJSX.push(<li><input onClick={this.props.onClick2[i]} type="checkbox" id="scales" name="scales"
-        ></input>Purchase Name: {this.props.receipt.lines[i].name}</li>)
+        ></input>Purchase Name: {this.props.receipt.lines[i].lineName}</li>)
 
         tempJSX.push(<p>Price Before Split: {this.props.receipt.lines[i].linePrice}</p>);
         tempJSX.push(<p>Equal Price Split:</p>);
 
               for (let j = 0; j < this.props.receipt.lines[i].items.length; j++)
               {
-                tempJSX.push(<div>{this.props.receipt.lines[i].name} pays ${this.props.receipt.lines[i].linePrice/(this.props.receipt.lines[i].items.length)}  </div>)
+                tempJSX.push(<div>{this.props.receipt.lines[i].items[j].itemClaimant.username} pays ${this.props.receipt.lines[i].linePrice/(this.props.receipt.lines[i].items.length)}  </div>)
               }
         
         receiptHTML.push(<div>{tempJSX}</div>)
